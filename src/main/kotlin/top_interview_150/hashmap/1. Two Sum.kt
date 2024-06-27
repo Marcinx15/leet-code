b01b9b7fa6d81805a1a0ab61ec4c81ed046117d7
Marcin Kurek <marcinkurek17@gmail.com>
2024-06-27 16:52:02 +0200

    test

--- src/main/kotlin/top_interview_150/hashmap/1. Two Sum.kt
+++ src/main/kotlin/top_interview_150/hashmap/1. Two Sum.kt
@@ -3,7 +3,6 @@ package top_interview_150.hashmap
 fun main() {
 }
 
-
 fun twoSum(nums: IntArray, target: Int): IntArray {
     val valueToIndex = mutableMapOf<Int, Int>()
     nums.forEachIndexed { index, value ->
