999ed972f3d4545aa206e59f3597818eb9211dd4
Marcin Kurek <marcinkurek17@gmail.com>
2024-08-07 00:11:02 +0200

    151 second solution

--- src/main/kotlin/top_interview_150/array/151. Reverse Words in a String.kt
+++ src/main/kotlin/top_interview_150/array/151. Reverse Words in a String.kt
@@ -3,9 +3,9 @@ package top_interview_150.array
 import java.util.Stack
 
 fun main() {
-    reverseWords("the sky is blue").let { println(it) }
-    reverseWords("  hello world  ").let { println(it) }
-    reverseWords("a good   example").let { println(it) }
+    reverseWords2("the sky is blue").let { println(it) }
+    reverseWords2("  hello world  ").let { println(it) }
+    reverseWords2("a good   example").let { println(it) }
 }
 
 fun reverseWords(s: String): String {
@@ -35,8 +35,27 @@ fun reverseWords(s: String): String {
     return wordBuilder.toString()
 }
 
+fun reverseWords2(s: String): String {
+    val reversedWords = ArrayDeque<String>()
+
+    var startIndex = 0
+    var endIndex = 0
+    while (startIndex < s.length) {
+        while (startIndex < s.length && s[startIndex] == ' ') startIndex++
+        if (startIndex == s.length) break
+
+        endIndex = startIndex + 1
+        while (endIndex < s.length && s[endIndex] != ' ') endIndex++
+
+        reversedWords.addFirst(s.substring(startIndex, endIndex))
+        startIndex = endIndex
+    }
+
+    return reversedWords.joinToString(separator = " ")
+}
+
 /*
 
-
+    "  hello world  "
 
  */
