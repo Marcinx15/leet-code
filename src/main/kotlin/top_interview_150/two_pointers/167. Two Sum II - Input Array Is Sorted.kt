fef98ec562332b9c46e56ccf3bebc3b723b09e44
Marcin Kurek <marcinkurek17@gmail.com>
2024-08-08 01:08:57 +0200

    167

--- src/main/kotlin/top_interview_150/two_pointers/167. Two Sum II - Input Array Is Sorted.kt
+++ src/main/kotlin/top_interview_150/two_pointers/167. Two Sum II - Input Array Is Sorted.kt
@@ -2,17 +2,31 @@ package top_interview_150.two_pointers
 
 fun main() {
     println(twoSum(numbers = intArrayOf(2,7,11,15), target = 9).toList())
+    println(twoSum(numbers = intArrayOf(2, 3, 4), target = 6).toList())
+    println(twoSum(numbers = intArrayOf(-1, 0), target = -1).toList())
 }
 
 fun twoSum(numbers: IntArray, target: Int): IntArray {
-    var startIndex = 0
-    var endIndex = numbers.lastIndex
-    while(true) {
-        val sum = numbers[startIndex] + numbers[endIndex]
+    var (left, right) = Pair(0, numbers.lastIndex)
+    while (left < right) {
+        val sum = numbers[left] + numbers[right]
         when {
-            sum < target -> startIndex++
-            sum > target -> endIndex--
-            else -> return intArrayOf(startIndex + 1, endIndex + 1)
+            sum == target -> return intArrayOf(left + 1, right + 1)
+            sum < target -> left++
+            else -> right--
         }
     }
+    return intArrayOf()
 }
+
+/*
+
+[2,7,11,12,15] target = 18
+[a,b,c,d,e,f,g,h,i,j,k,l,m,n]
+
+    c + l > target
+
+    b + m < target || b + n < target => b + l < target
+
+
+ */
