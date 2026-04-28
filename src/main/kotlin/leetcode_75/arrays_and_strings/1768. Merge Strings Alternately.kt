df2177557ad963a59c3e7be1040407e39b5dedd2
Marcinx15 <marcinkurek17@gmail.com>
2026-04-28 17:36:20 +0200

    Start LeetCode 75

---
+++ src/main/kotlin/leetcode_75/arrays_and_strings/1768. Merge Strings Alternately.kt
@@ -0,0 +1,32 @@
+package leetcode_75.arrays_and_strings
+
+import kotlin.math.min
+
+fun main() {
+    val solution = Solution()
+    val testCases = listOf(
+        "abc" to "pqr",
+        "ab" to "pqrs",
+        "abcd" to "pq"
+    )
+
+    testCases.forEach {
+        println(solution.mergeAlternately(it.first, it.second))
+    }
+}
+
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val builder = StringBuilder()
+        val minLength = min(word1.length, word2.length)
+        (0 until minLength).forEach { idx ->
+            builder.append(word1[idx])
+            builder.append(word2[idx])
+        }
+
+        if (word1.length > word2.length) builder.append(word1.substring(minLength))
+        else if(word1.length < word2.length) builder.append(word2.substring(minLength))
+
+        return builder.toString()
+    }
+}
