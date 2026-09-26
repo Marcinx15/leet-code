3238b663ba6d1ec0db21bd522c195fc1373f3648
Marcinx15 <marcinkurek17@gmail.com>
2026-09-26 12:18:12 +0200

    700

--- src/main/kotlin/leetcode_75/700. Search in a Binary Search Tree.kt
+++ src/main/kotlin/leetcode_75/700. Search in a Binary Search Tree.kt
@@ -1,2 +1,29 @@
 package leetcode_75
 
+fun main() {
+    val node1 = TreeNode(4)
+    val node2 = TreeNode(2)
+    val node3 = TreeNode(7)
+    val node4 = TreeNode(1)
+    val node5 = TreeNode(3)
+
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+
+    val result = searchBST(node1, 10)
+    println(result?.value)
+    println(result?.left?.value)
+    println(result?.right?.value)
+
+}
+
+fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+    return when {
+        root == null -> null
+        root.value == `val` -> root
+        root.value < `val` -> searchBST(root.right, `val`)
+        else -> searchBST(root.left, `val`)
+    }
+}
